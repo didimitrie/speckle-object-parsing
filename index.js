5593c6558c5b99c26df6c85afdf20a2a90007aab
Dimitrie Stefanescu <didimitrie@gmail.com>
2022-05-31 13:06:57 +0100

    feat: extra processing step details

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+// Nodejs (backend) parsing of objects example relying on the speckle object loader.
+
+
 import { fetch } from 'undici'
 import ObjectLoader from '@speckle/objectloader'
 
@@ -6,11 +10,12 @@ const loader = new ObjectLoader({
   streamId: '0ec74dbb28',
   objectId: '6e5fd39d216302666c84a918a9ea6b18',
   // token: 'xxx' // use here the users's api token
-  options: { enableCaching: false, fetch }
+  options: { enableCaching: false, fetch } // note, if used in the browser, this line shoud go away!
 })
 
 let total = 0
 let count = 0 
+let allObjects = []
 
 let totalExteriorWallArea = 0
 let totalExteriorCurtainWallArea = 0
@@ -19,18 +24,53 @@ let totalInteriorWallArea = 0
 let exteriorWallTypes = ['Wall - Timber Clad', 'SIP 202mm Wall - conc clad']
 let curtainWallTypes = ['SH_Curtain wall']
 
+let extWallElementIds = []
+let intWallElementIds = []
+
+// first pass: we iterate through all the objects as they get received from the api.
 for await ( let obj of loader.getObjectIterator() ) {
+  allObjects.push(obj)
   if( !total ) total = obj.totalChildrenCount // set the total children count from the first object
-  console.log( `Progress: ${count++}/${total}` )
 
   if(obj.speckle_type === 'Objects.BuiltElements.Wall:Objects.BuiltElements.Revit.RevitWall') {
     if(exteriorWallTypes.indexOf(obj.type) !== -1 ) {
-      const area = obj.baseLine.length * obj.height // one way of calculating the area - a bit more basic
-      totalExteriorWallArea += area
-    } else if(curtainWallTypes.indexOf(obj.type !== -1)) {
-      console.log(obj.parameters.HOST_AREA_COMPUTED)
+      // const area = obj.baseLine.length * obj.height // one way of calculating the area - a bit more basic, and does not take into account window openings.
+      totalExteriorWallArea += obj.parameters.HOST_AREA_COMPUTED.value  // another way of getting the area by relying on the revit prop itself; more reliable.
+
+      // TODO: go through the obj.elements array, where the references to windows and doors are stored
+      if(obj.elements) {
+        extWallElementIds.push( ...obj.elements.map(el => el.referencedId))
+      }
+    } else if(curtainWallTypes.indexOf(obj.type) !== -1) {
+      totalExteriorCurtainWallArea += obj.parameters.HOST_AREA_COMPUTED.value 
+    } else {
+      // TODO: Assume interior wall
+      totalInteriorWallArea += obj.parameters.HOST_AREA_COMPUTED.value
+      if(obj.elements) {
+        intWallElementIds.push( ...obj.elements.map(el => el.referencedId))
+      }
     }
   }
 
+  // TODO: if roof
+
+  // TODO: if slab
+  // - add area to total floor area
+  // - check for footprint (by level? by lowest centroid?)
+
+  // TODO: if level, push to separate list and process afterwards for average floor height? 
+
 }
+
+for(let id of extWallElementIds) {
+  // TODO: push in ext door areas, ext window area
+}
+
+for(let id of intWallElementIds) {
+  // TODO: push in int door areas, ext window area
+}
+
+console.log(`Ext wall area: ${totalExteriorWallArea} m2`)
+console.log(`Curtain wall area: ${totalExteriorCurtainWallArea} m2`)
+console.log(`Int wall area: ${totalInteriorWallArea} m2`)
 
