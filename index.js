2bf9cbd48df27a4b211c9b85331ed489c14cccbb
Dimitrie Stefanescu <didimitrie@gmail.com>
2022-05-31 13:15:43 +0100

    feat: extra filtering

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Nodejs (backend) parsing of objects example relying on the speckle object loader.
 
 
-import { fetch } from 'undici'
+import { fetch } from 'undici' // you do not need this in the browser, as speckle will rely on the default fetch
 import ObjectLoader from '@speckle/objectloader'
 
 const loader = new ObjectLoader({
@@ -14,23 +14,29 @@ const loader = new ObjectLoader({
 })
 
 let total = 0
-let count = 0 
+let processedCount = 0 
 let allObjects = []
+let ignoredSpeckleTypes = ['Objects.Geometry.Mesh', 'Objects.Other.RenderMaterial', 'Speckle.Core.Models.DataChunk']
 
 let totalExteriorWallArea = 0
 let totalExteriorCurtainWallArea = 0
 let totalInteriorWallArea = 0
 
-let exteriorWallTypes = ['Wall - Timber Clad', 'SIP 202mm Wall - conc clad']
-let curtainWallTypes = ['SH_Curtain wall']
+let exteriorWallTypes = ['Wall - Timber Clad', 'SIP 202mm Wall - conc clad'] // Note: these should most likely be user inputs
+let curtainWallTypes = ['SH_Curtain wall'] // Note: these should most likely be user inputs
+let intWallTypes = [] // Not used, we assume any other wall that is not anything from the above is an interior wall
 
-let extWallElementIds = []
-let intWallElementIds = []
+let extWallChildElementIds = []
+let intWallChildElementIds = []
 
+console.log('First processing step start (main elements)')
 // first pass: we iterate through all the objects as they get received from the api.
 for await ( let obj of loader.getObjectIterator() ) {
-  allObjects.push(obj)
   if( !total ) total = obj.totalChildrenCount // set the total children count from the first object
+  processedCount++ 
+
+  if(ignoredSpeckleTypes.indexOf(obj.speckle_type) !== -1) // ignore objects we know we don't need
+    allObjects.push(obj)
 
   if(obj.speckle_type === 'Objects.BuiltElements.Wall:Objects.BuiltElements.Revit.RevitWall') {
     if(exteriorWallTypes.indexOf(obj.type) !== -1 ) {
@@ -39,7 +45,7 @@ for await ( let obj of loader.getObjectIterator() ) {
 
       // TODO: go through the obj.elements array, where the references to windows and doors are stored
       if(obj.elements) {
-        extWallElementIds.push( ...obj.elements.map(el => el.referencedId))
+        extWallChildElementIds.push( ...obj.elements.map(el => el.referencedId))
       }
     } else if(curtainWallTypes.indexOf(obj.type) !== -1) {
       totalExteriorCurtainWallArea += obj.parameters.HOST_AREA_COMPUTED.value 
@@ -47,26 +53,28 @@ for await ( let obj of loader.getObjectIterator() ) {
       // TODO: Assume interior wall
       totalInteriorWallArea += obj.parameters.HOST_AREA_COMPUTED.value
       if(obj.elements) {
-        intWallElementIds.push( ...obj.elements.map(el => el.referencedId))
+        intWallChildElementIds.push( ...obj.elements.map(el => el.referencedId))
       }
     }
   }
 
   // TODO: if roof
+  // Note, roofs contain windows that need to be pushed by ids in "extWallChildElementIds"
 
   // TODO: if slab
   // - add area to total floor area
-  // - check for footprint (by level? by lowest centroid?)
+  // - check for building footprint (by level? by lowest centroid?)
 
   // TODO: if level, push to separate list and process afterwards for average floor height? 
 
 }
+console.log('Second processing step start (windows, doors)')
 
-for(let id of extWallElementIds) {
+for(let id of extWallChildElementIds) {
   // TODO: push in ext door areas, ext window area
 }
 
-for(let id of intWallElementIds) {
+for(let id of intWallChildElementIds) {
   // TODO: push in int door areas, ext window area
 }
 
